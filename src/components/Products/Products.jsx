e16acd4e2430d21bcff2585456cd6186dd84dea1
Doven <doven.obertinca@gmail.com>
2022-03-01 10:37:43 +0100

    with some articles

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
-import { Grid } from "@material-ui/core";
+import Grid from '@material-ui/core/Grid';
 import Product from './Product/Product'
 import useStyles from './styles';
 
-const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes.', price: '5€ ', image: '/src/assets/feet-1840619_1920.jpg'},
-    {id: 2, name: 'Macbook', description: 'Apple macbook.', price: '10€ ', image: "https://pixabay.com/photos/desk-laptop-notebook-pen-workspace-593327"},
+// const products = [
+//     {id: 1, name: 'Shoes', description: 'Running shoes.', price: '5€ ', image: '/src/assets/feet-1840619_1920.jpg'},
+//     {id: 2, name: 'Macbook', description: 'Apple macbook.', price: '10€ ', image: "https://pixabay.com/photos/desk-laptop-notebook-pen-workspace-593327"},
 
-]
+// ]
 
 
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
     const classes = useStyles();
-    return(
-        <main className={classes.content}>
-            <div className={classes.toolbar} />
+  
+    if (!products.length) return <p>Loading...</p>;
+  
+    return (
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Grid container justify="center" spacing={4}>
-            {products.map((product) =>(
-                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product product={product} />
-                </Grid>
-            ))}
+          {products.map((product) => (
+            <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+              <Product product={product} onAddToCart={onAddToCart} />
+            </Grid>
+          ))}
         </Grid>
-    </main>
-    )
-    
-}
+      </main>
+    );
+  };
 
 
 export default Products;

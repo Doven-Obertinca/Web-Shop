96388b755f3691812cc3de205bd041fa1a83d428
Doven <doven.obertinca@gmail.com>
2022-03-07 12:51:50 +0100

    adding links

--- src/components/Products/Navbar/Navbar.jsx
+++ src/components/Products/Navbar/Navbar.jsx
@@ -4,27 +4,31 @@ import { ShoppingCart } from '@material-ui/icons';
 import { mergeClasses } from '@material-ui/styles';
 import logo from "../../../assets/cherry-575547_1280.png";
 import useStyles from './styles'
-
+import { Link, useLocation } from 'react-router-dom';
 
 function Navbar({ totalItems }) {
   const classes = useStyles()
+  const location = useLocation();
+
+  
   return (
    <>
       <AppBar position="fixed" className={classes.appBar} color="inherit">
           <Toolbar>
-            <Typography variant='h6' className={classes.title} color="inherit">
+            <Typography component={Link} to="/" variant='h6' className={classes.title} color="inherit">
                 <img src={logo} alt="commerce.js" height="25px" className={classes.image} />
                 My Shop
             </Typography>
             <div className={classes.grow} />
+            {location.pathname ==="/" &&(
             <div className={classes.button}>
-              <IconButton aria-label="Show cart items" color="iherit">
+              <IconButton component={Link} to="/cart" aria-label="Show cart items" color="iherit">
                 <Badge badgeContent={totalItems} color="secondary">
                   <ShoppingCart />
                 </Badge>
 
               </IconButton>
-            </div>
+            </div>)}
           </Toolbar>
         
         </AppBar> 
